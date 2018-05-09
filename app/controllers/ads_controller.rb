70d08c87eb471e2df7335100a5c2612a647f5f70
DanChan23 <38598772+DanChan23@users.noreply.github.com>
2018-05-09 10:33:57 +0800

    Update ads_controller.rb
    changed the function update and added def ad_params

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -1,42 +1,41 @@
 class AdsController < ApplicationController
-    def index
-        @all_ads = Ad.all
-    end
-    
-    def show
-        @ad = Ad.find(params[:id])
-    end
+	def index
+		@ads = Ad.all
+	end
 
-    def new
-        @ad = Ad.new
-        
-    end
-    
-    def create
-        @ad = Ad.new
-        @ad.name = params[:ad][:name]
-        @ad.price = params[:ad][:price]
-        @ad.save
-        
-        redirect_to "/ads/#{@ad.id}"
-    end
+	def show
+		@ad = Ad.find(params[:id])
+	end
 
-    def delete
-        @ad = Ad.find(params[:id])
-        @ad.destroy
-        redirect_to "/ads/"
-    end
-    
-    def edit
-        @ad = Ad.find(params[:id])
-    end
-    
-    def update
-        @ad = Ad.new
-        @ad.name = params[:ad][:name]
-        @ad.price = params[:ad][:price]
-        @ad.update
-    end
-    
+	def new
+		@ad = Ad.new
+	end
 
+	def create
+		@ad = Ad.new
+		@ad.attributes = ad_params
+		@ad.save
+
+		redirect_to "/ads/#{@ad.id}"
+	end
+
+	def edit
+		@ad = Ad.find(params[:id])
+	end
+
+	def update
+		@ad = Ad.find(params[:id])
+		@ad.update(ad_params)
+	end
+
+	def delete
+		@ad = Ad.find(params[:id])
+		@ad.destroy
+
+		redirect_to "/ads/"
+	end
+
+	def ad_params
+		params.require(:ad).permit(:name, :price)
+	end
 end
